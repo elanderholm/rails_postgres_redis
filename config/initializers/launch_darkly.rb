80a54f58ac6eb0cb1ce7caaac7e78f8efd256f33
erik landerholm <elanderholm@gmail.com>
2020-11-25 10:22:36 -0800

    rake task to create launch darkly env

--- config/initializers/launch_darkly.rb
+++ config/initializers/launch_darkly.rb
@@ -1,7 +1,6 @@
-LAUNCH_DARKLY_API_KEY = ENV['LAUNCH_DARKLY_API_KEY'] || 'api-ce00e0a5-8be8-495a-959e-e1a39639410a'
-LAUNCH_DARKLY_PROJECT_NAME = ENV['LAUNCH_DARKLY_PROJECT_NAME'] || 'default'
+LAUNCH_DARKLY_API_KEY = ENV['LAUNCH_DARKLY_API_KEY']
+LAUNCH_DARKLY_PROJECT_NAME = ENV['LAUNCH_DARKLY_PROJECT_NAME']
 LAUNCH_DARKLY_ENV_NAME = ENV['RELEASE_ENV_ID']
-
 LaunchDarklyApi.configure do |config|
   config.api_key['Authorization'] = LAUNCH_DARKLY_API_KEY
   config.debugging = true
@@ -12,9 +11,8 @@ begin
     project: LAUNCH_DARKLY_PROJECT_NAME,
     env: LAUNCH_DARKLY_ENV_NAME  
   )
-  Spring.after_fork do
-    Rails.configuration.ld_client = LaunchDarkly::LDClient.new(launch_darkly_env.apiKey)
-  end
+  
+  $ld_client = Rails.configuration.ld_client = LaunchDarkly::LDClient.new(launch_darkly_env.apiKey)
 rescue => e
   binding.pry
 end
