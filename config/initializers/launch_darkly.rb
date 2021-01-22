871bc12b92d70dcf9f173d6c210d130aa47bcd13
erik landerholm <elanderholm@gmail.com>
2021-01-21 19:08:18 -0800

    use the envs from release

--- config/initializers/launch_darkly.rb
+++ config/initializers/launch_darkly.rb
@@ -1,21 +1,10 @@
-LAUNCH_DARKLY_API_KEY = ENV['LAUNCH_DARKLY_API_KEY']
-LAUNCH_DARKLY_PROJECT_NAME = ENV['LAUNCH_DARKLY_PROJECT_NAME']
-LAUNCH_DARKLY_ENV_NAME = ENV['RELEASE_ENV_ID']
+access_token = ENV['LAUNCHDARKLY_ACCESS_TOKEN']#'api-ce00e0a5-8be8-495a-959e-e1a39639410a' #ENV['LAUNCH_DARKLY_ACCESS_TOKEN']
+
+##SETUP auth using access token
 LaunchDarklyApi.configure do |config|
-  config.api_key['Authorization'] = LAUNCH_DARKLY_API_KEY
+  config.api_key['Authorization'] = access_token
   config.debugging = true
 end
 
-begin
-  launch_darkly_env = Ld.get_environment(
-    project: LAUNCH_DARKLY_PROJECT_NAME,
-    env: LAUNCH_DARKLY_ENV_NAME  
-  )
-  
-  Rails.configuration.ld_client = LaunchDarkly::LDClient.new(launch_darkly_env.apiKey)
-rescue => e
-  Rails.logger.error "ERROR!!"
-  Rails.logger.error "LAUNCH_DARKLY_API_KEY : #{LAUNCH_DARKLY_API_KEY}"
-  Rails.logger.error "LAUNCH_DARKLY_PROJECT_NAME : #{LAUNCH_DARKLY_PROJECT_NAME}"
-  Rails.logger.error "LAUNCH_DARKLY_ENV_NAME : #{LAUNCH_DARKLY_ENV_NAME}"
-end
+##et ld_client to use the api key for the specific environment
+Rails.configuration.ld_client = LaunchDarkly::LDClient.new(ENV['LAUNCHDARKLY_ENV_SPECIFIC_SDK_KEY'])
